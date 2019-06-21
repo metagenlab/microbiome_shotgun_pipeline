bc8084f14fd3e945cbebb816e76e9b0ce21e5368
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-06-21 12:18:45 +0200

    added annotation and genus to count table

--- rules/protein_abundance/scripts/calculate_RPKM.py
+++ rules/protein_abundance/scripts/calculate_RPKM.py
@@ -7,7 +7,29 @@ import pandas
 conn = sqlite3.connect(snakemake.output[0])
 cursor = conn.cursor()
 
+sql = 'attach "%s" as virulencedb' % snakemake.input["virulence_database"]
+cursor.execute(sql)
+
+sql = 'create table uniparc_accession2genus (accession varchar(200), genus varchar(400))'
+cursor.execute(sql)
+
 sample_table = snakemake.params[0]
+
+uniparc_accession2silix_acc_and_consensus_annotation = {}
+sql = 'select uniparc_accession,silix_name,description from uniparc2silix_90 t1 inner join uniparc_consensus_annotation t2 on t1.uniparc_id=t2.uniparc_id;'
+for row in cursor.execute(sql).fetchall():
+    row = list(row)
+    uniparc_accession2silix_acc_and_consensus_annotation[row[0]] = row[1:]
+
+uniparc_accession2genus_list = {}
+sql = 'select distinct uniparc_accession,genus_name from uniparc2silix_90 t1 inner join uniparc2species t2 on t1.uniparc_id=t2.uniparc_id;'
+for row in cursor.execute(sql).fetchall():
+    row = list(row)
+    if row[0] not in uniparc_accession2genus_list:
+        uniparc_accession2genus_list[row[0]] = [row[1]]
+    else:
+        uniparc_accession2genus_list[row[0]].append(row[1])
+
 
 all_samples = pandas.read_csv(sample_table, sep="\t", index_col=0)
 
@@ -46,11 +68,13 @@ for sample in snakemake.input["sample_list"]:
             else:
                 sample2sequence_accession2count[sample_id][data[1]] += 1
 
-sql = 'create table sequence_counts (sample varchar(200), accession varchar(200), n_hits INTEGER, RPKM INTEGER, group_1 varchar(200), group_2 varchar (200))'
+sql = 'create table sequence_counts (sample varchar(200), accession varchar(200), n_hits INTEGER, RPKM INTEGER, group_1 varchar(200), group_2 varchar (200), silix_accession varchar(200), annotation TEXT)'
+
+sql_taxonomy = 'insert into uniparc_accession2genus values (?, ?)'
 cursor.execute(sql,)
 
 # load data into database
-sql_template = 'insert into sequence_counts values (?, ?, ?, ?, ?, ?)'
+sql_template = 'insert into sequence_counts values (?, ?, ?, ?, ?, ?, ?, ?)'
 for sample in sample2sequence_accession2count:
     for sequence_accession in sample2sequence_accession2count[sample]:
         n_hits = sample2sequence_accession2count[sample][sequence_accession]
@@ -66,18 +90,28 @@ for sample in sample2sequence_accession2count:
         denominat = seq_length_nucl * reads_millions
 
         # calculate ratio
-        print("calculate ratio")
         RPKM = float(nominat)/float(denominat)
 
+        silix_acc = uniparc_accession2silix_acc_and_consensus_annotation[sequence_accession][0]
+        description = uniparc_accession2silix_acc_and_consensus_annotation[sequence_accession][1]
         cursor.execute(sql_template, (sample,
                                       sequence_accession,
                                       n_hits,
                                       RPKM,
                                       all_samples.loc[sample, "group_1"],
-                                      all_samples.loc[sample, "group_2"]))
+                                      all_samples.loc[sample, "group_2"],
+                                      silix_acc,
+                                      description))
+        genus_list = uniparc_accession2genus_list[sequence_accession]
+        for genus in genus_list:
+            cursor.execute(sql_taxonomy, [sequence_accession, genus])
     conn.commit()
 
 # index sequence accession
 sql_index_1 = 'create index acc on sequence_counts (accession);'
 cursor.execute(sql_index_1,)
+
+sql_index_2 = 'create index acc2 on uniparc_accession2genus (accession);'
+cursor.execute(sql_index_2,)
+
 conn.commit()
