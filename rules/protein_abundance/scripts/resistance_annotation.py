80efd0f96a9ca3db4d85a6b6e895789e9c91e1eb
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-08-21 15:08:44 +0200

    check for missing arp metadata

--- rules/protein_abundance/scripts/resistance_annotation.py
+++ rules/protein_abundance/scripts/resistance_annotation.py
@@ -30,14 +30,20 @@ cursor.execute(sql)
 card_accession_list = [i[0] for i in cursor.fetchall()]
 
 print(aro)
-
+problems = 0
 for accession in card_accession_list:
     aro_accession = aro_index.loc[accession, "ARO Accession"]
     aro_name = aro.loc[aro_accession, "Name"]
     aro_description = aro.loc[aro_accession, "Description"]
-    resistance_mechanism = aro_categories_index.loc[accession, "Resistance Mechanism"]
-    AMR_family = aro_categories_index.loc[accession, "AMR Gene Family"]
-    drug_class = aro_categories_index.loc[accession, "Drug Class"]
-    print(accession)
-    print(drug_class)
-    print(resistance_mechanism)
+    try:
+        resistance_mechanism = aro_categories_index.loc[accession, "Resistance Mechanism"]
+        AMR_family = aro_categories_index.loc[accession, "AMR Gene Family"]
+        drug_class = aro_categories_index.loc[accession, "Drug Class"]
+        #print(accession)
+        # macrolide antibiotic;fluoroquinolone antibiotic;aminoglycoside antibiotic;lincosamide antibiotic;carbapenem;fosfomycin;cephalosporin;glycylcycline;bicyclomycin;penam;nucleoside antibiotic;tetracycline antibiotic;peptide antibiotic;acridine dye;oxazolidinone antibiotic;rifamycin antibiotic;diaminopyrimidine antibiotic;phenicol antibiotic;isoniazid;penem;benzalkonium chloride;rhodamine;antibacterial free fatty acids;nitroimidazole antibiotic
+        #print(drug_class)
+        #print(resistance_mechanism)
+    exept: 
+        print("problem with", aro_accession)
+        problems+=1
+print(problems)
