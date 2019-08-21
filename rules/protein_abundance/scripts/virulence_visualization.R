ce2a2df9f0e44fb02e45205cc5d9060d9951547f
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-08-21 17:27:50 +0200

    plot 2

--- rules/protein_abundance/scripts/virulence_visualization.R
+++ rules/protein_abundance/scripts/virulence_visualization.R
@@ -6,8 +6,11 @@ library("svglite")
 library("dplyr")
 library("tibble")
 
+
+# get sample table
+sample_table <- read.table(snakemake@params[["sample_table"]])
+
 # get table
-print(snakemake@input[["database"]])
 con <- dbConnect(RSQLite::SQLite(), snakemake@input[["database"]])
 res <- dbSendQuery(con, "SELECT * FROM sequence_counts")
 rpkm_table <- dbFetch(res)
@@ -26,14 +29,30 @@ rpkm_log2_table_dcast <- t(rpkm_log2_table_dcast[,2:length(rpkm_log2_table_dcast
 
 
 # PLOT 1 number of VFs
-
 res <- dbSendQuery(con, "SELECT sample,group_2,group_1,count(*) as n FROM sequence_counts group by sample,group_2,group_1")
 table_counts <- dbFetch(res)
-
-fact <- c("Amsterdam_RAW", "Amsterdam_TREATED", "Evry_RAW", "Evry_TREATED", "Gava_RAW_S1", "Gava_RAW_S2", "Gava_TREATED_S1", "Gava_TREATED_S2", "Prat_RAW", "Prat_TREATED" )
 
 p <- ggplot(data=table_counts, aes(x=sample, y=n, fill=group_2))
 p <- p + geom_bar(stat="identity")
 p <- p+ theme(axis.text.x = element_text(angle = 90))+ facet_grid(. ~ group_1, scales="free")
 
 ggsave(snakemake@output[["plot1"]], p, height=5, width=8)
+
+# PLOT 2
+pdf(snakemake@output[["plot1"]], height=9, width=5)
+for (i in unique(sample_table$group_1)) { 
+    res <- dbSendQuery(con, paste('select group_2,genus,count(*) as n from (select distinct sample,t1.accession,group_2,genus from sequence_counts t1 inner join uniparc_accession2genus t2 on t1.accession=t2.accession where group_1="', i ,'") A group by group_2,A.genus order by n DESC;'))
+    table_genus <- dbFetch(res)
+    table_genus_dcast <- dcast(table_genus, genus~group_2, value.var="n")
+    table_genus_dcast[is.na(table_genus_dcast)] <- 0
+
+    dendro <- as.dendrogram(hclust(d = dist(x = as.matrix(table_genus_dcast))))
+    order <- order.dendrogram(dendro)
+    table_genus$genus <- factor(x = table_genus$genus,
+                                levels = table_genus_dcast[,1][order], 
+                                ordered = TRUE)
+
+    p <- ggplot(table_genus , aes(group_2, genus)) + geom_tile(aes(fill = n)) + scale_fill_gradient(low = "yellow", high = "steelblue")
+    p <- p + theme(axis.text.x = element_text(angle = 90))
+    print(p)
+}
