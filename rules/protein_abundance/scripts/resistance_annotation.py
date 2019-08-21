aa6e47834440aa942d3bc5cff1fb35359d3da333
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-08-21 14:47:35 +0200

    added rule for resistance annotations

--- rules/protein_abundance/scripts/resistance_annotation.py
+++ rules/protein_abundance/scripts/resistance_annotation.py
@@ -8,9 +8,9 @@ import pandas
 conn = sqlite3.connect(snakemake.input["RPKM_database"])
 cursor = conn.cursor()
 
-protein_accession2aro_index = pandas.read_csv(snakemake.input["aro_index"], delimiter='\t').to_dict()["Protein Accession"]
-aro_accession2description = pandas.read_csv(snakemake.input["aro"], delimiter='\t').to_dict()["Accession"]
-protein_accession2aro_categories_index = pandas.read_csv(snakemake.input["aro_categories_index"], delimiter='\t').to_dict()["Protein Accession"]
+aro_index = pandas.read_csv(snakemake.input["aro_index"], delimiter='\t').set_index("Protein Accession")
+aro = pandas.read_csv(snakemake.input["aro"], delimiter='\t').set_index("Accession")
+aro_categories_index = pandas.read_csv(snakemake.input["aro_categories_index"], delimiter='\t').set_index("Protein Accession")
 RPKM_database = snakemake.input["RPKM_database"]
 
-print(aro_accession2description)
+print(aro)
