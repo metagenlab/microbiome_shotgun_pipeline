99aa64f5cc5dae2b8902cde346c5501ad4db97c0
idfarbanecha <faridchaabane23@gmail.com>
2019-10-21 19:56:12 +0200

    added script to split cov file into seperate ones

---
+++ rules/taxonomy_profiling/scripts/Ezvir/coverage-parse.py
@@ -0,0 +1,19 @@
+import dask.dataframe as df
+
+import os
+tb=df.read_csv(snakemake.input[0],sep='\t',names=['fasta_header','nucleotide_pos','nucleotide_cov']).compute()
+gb=tb.groupby(['fasta_header','nucleotide_pos'],as_index=False)['nucleotide_cov'].sum()
+gb.index=gb['fasta_header']
+indexes=list(set(gb.index))#list of unique indexes/fasta files
+dataframe_dic={}
+
+os.mkdir(snakemake.params[0])
+for index in indexes:
+    table=gb.loc[gb['fasta_header']==index]
+    head=index.split('|')
+    acc=head[len(head)-2].replace('.','_')
+    path=os.path.join(snakemake.params[0],f"{acc}.csv")
+    table.to_csv(path,header=None,index=False,sep='\t')
+
+
+
