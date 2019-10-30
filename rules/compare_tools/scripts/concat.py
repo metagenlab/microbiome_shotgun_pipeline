f92815e7d1636b98c1930285ec4e275b9a59da14
idfarbanecha <faridchaabane23@gmail.com>
2019-10-30 18:17:43 +0100

    seperated vir and bac tables

--- rules/compare_tools/scripts/concat.py
+++ rules/compare_tools/scripts/concat.py
@@ -1,10 +1,22 @@
 import pandas as pd
-table_list=[]
-for file in snakemake.input:
-    table=pd.read_csv(file,sep='\t',index_col=0)
-    table.index.name = 'superkingdom'
-    table_list.append(table)
 
-all_tools=pd.concat(table_list,sort=False,axis=0)
-all_tools=all_tools.sort_values(by='F1_score',ascending=False)
-all_tools.to_csv(snakemake.output[0],sep='\t')
+def concat_tables(file_list):
+    table_list = []
+    for file in file_list:
+        table=pd.read_csv(file,sep='\t',index_col=0)
+        table.index.name = 'tool'
+        table_list.append(table)
+    all_tools = pd.concat(table_list, sort=False, axis=0)
+    if 'F1_score' in all_tools.columns:
+        all_tools=all_tools.sort_values(by='F1_score',ascending=False)
+    return all_tools
+
+vir_scores=concat_tables(snakemake.input.vir_scores)
+vir_scores.to_csv(snakemake.output.all_vir_scores,sep='\t')
+vir_presence=concat_tables(snakemake.input.vir_presence)
+vir_presence.to_csv(snakemake.output.all_vir_presence,sep='\t')
+bac_scores=concat_tables(snakemake.input.bac_scores)
+bac_scores.to_csv(snakemake.output.all_bac_scores,sep='\t')
+bac_presence=concat_tables(snakemake.input.bac_presence)
+bac_presence.to_csv(snakemake.output.all_bac_presence,sep='\t')
+
