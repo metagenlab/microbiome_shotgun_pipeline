f3eb30e9b7b01a05c17fec2a59a93485f9b0b150
idfarbanecha <faridchaabane23@gmail.com>
2019-10-30 10:33:40 +0100

    corrected selection at rank level

--- rules/compare_tools/scripts/stats.py
+++ rules/compare_tools/scripts/stats.py
@@ -14,11 +14,11 @@ def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name, superkingdom):
 
     tool_tb = tool_tb[tool_tb.superkingdom == superkingdom]
     true_tb = true_tb[true_tb.superkingdom == superkingdom]
-    names = list(true_tb['species'])
-    taxids = list(true_tb['species_taxid'])
+    names = list(true_tb[f'{rank}'])
+    taxids = list(true_tb[f'{rank}_taxid'])
     taxid2names = dict(zip(taxids, names))
-    true_list = list(true_tb.groupby([f'{rank}_taxid'], as_index=False).sum()['species_taxid'])
-    tool_list = list(tool_tb.groupby([f'{rank}_taxid'], as_index=False).sum()['species_taxid'])
+    true_list = list(true_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
+    tool_list = list(tool_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
 
     tp_list = [i for i in true_list if i in true_list and i in tool_list]
     fp_list = [i for i in tool_list if i not in true_list and i in tool_list]
@@ -43,6 +43,22 @@ def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name, superkingdom):
     df = pd.DataFrame.from_dict(scores, orient='index')
     return df
 
+
+def gold_standard_confusion_table(true_tb,tool_tb,rank,tool_name,superkingdom):
+    tool_tb = tool_tb.replace(np.nan, 'NA')
+    true_tb = true_tb.replace(np.nan, 'NA')
+
+    tool_tb = tool_tb[tool_tb.superkingdom == superkingdom]
+    true_tb = true_tb[true_tb.superkingdom == superkingdom]
+    names = list(true_tb[f'{rank}'])
+    taxids = list(true_tb[f'{rank}_taxid'])
+    taxid2names = dict(zip(taxids, names))
+    true_list = list(true_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
+    tool_list = list(tool_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
+    tp_list = [i for i in true_list if i in true_list and i in tool_list]
+    fn_list = [i for i in true_list if i in true_list and i not in tool_list]
+
+    return
 
 gold_standard=pd.read_csv(snakemake.input.gold_standard,sep='\t')
 tool_output=pd.read_csv(snakemake.input.tool_out,sep='\t')
--- rules/databases/ganon/generate_tab_ganon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-####Parameters
-input=$1
-output=$2
-
-####Main
-awk -F "\t" 'BEGIN {FS="\t";OFS="\t"}{if($4!="na"){print $4,$5,$6,$2}}' "$input" > "$output"
