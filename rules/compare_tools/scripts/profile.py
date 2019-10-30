d48cb54dc93b04fc41f72aec207e7c1ae836f930
idfarbanecha <faridchaabane23@gmail.com>
2019-10-30 09:16:26 +0100

    updated script to match new taxonomic table format

--- rules/compare_tools/scripts/profile.py
+++ rules/compare_tools/scripts/profile.py
@@ -1,65 +1,43 @@
 from ete3 import NCBITaxa
-from datetime import date
 import pandas as pd
-#import numpy as np
-today=date.today()
-d=today.strftime("%d.%m.%Y")
+import numpy as np
 ncbi = NCBITaxa()
-
-
 def get_lin_tax(table,ncbi,target_ranks):
-    taxid_list=table['Taxid']
-    counts=table['SimReads']
+    taxid_list=table['taxid']
+    counts=table['read_counts']
     count_dic = dict(zip(taxid_list, counts))
     tax={}
     for taxid in taxid_list:
         scientific_name=ncbi.translate_to_names([taxid])[0]
-        tax[taxid]={'scientific_name':scientific_name,'read_counts':count_dic[taxid]}
-        lineage=ncbi.get_lineage(taxid)
+        tax[taxid]={'taxid':str(taxid),'scientific_name':scientific_name,'read_counts':count_dic[taxid]}
+        lineage = ncbi.get_lineage(taxid)
         names = ncbi.get_taxid_translator(lineage)
-        ranks=ncbi.get_rank(lineage)
-        path=[]
+        ranks = ncbi.get_rank(lineage)
+        rank_list = list(ranks.values())
+        name_list = list(names.values())
+        rank2names = dict(zip(rank_list, name_list))
+
         for sub_taxid in lineage:
-            rank=ranks[sub_taxid]
-            name=names[sub_taxid]
+            rank = ranks[sub_taxid]
             if rank in target_ranks:
-                tax[taxid][rank]=name
-                path.append(str(sub_taxid))
-        last_taxid=path[len(path)-1]
-        last_rank=ranks[int(last_taxid)]
-        tax[taxid]['last_taxid']=last_taxid
-        tax[taxid]['last_rank']=last_rank
-        tax[taxid]['taxid_path']='|'.join(path)
-    df=pd.DataFrame.from_dict(tax, orient='index')
-    df.index.name='taxid'
+                tax[taxid][f"{rank}_taxid"] = sub_taxid
+        for n, rank in enumerate(target_ranks):
+            if rank in rank2names.keys():
+                tax[taxid][rank] = rank2names[rank]
+            else:
+                for ranks_to_skip in range(1, len(target_ranks) + 1):
+                    previous_rank = target_ranks[n - ranks_to_skip]
+                    if previous_rank in rank2names.keys():
+                        previous_name = rank2names[previous_rank]
+                        break
+                    if previous_rank not in rank2names.keys():
+                        continue
+                tax[taxid][rank] = f'{previous_name}_{rank[0:1]}'
+    df = pd.DataFrame.from_dict(tax, orient='index')
+    df = df.replace(np.nan, 'NA')
     return df
 target_ranks = ['superkingdom','phylum','order','family','genus','species']
 table=pd.read_csv(snakemake.input[0],sep='\t')
+table.columns=['names','read_counts','taxid','superkingdom','phylum','order','family','genus','species']
 lin_tax_tab=get_lin_tax(table,ncbi,target_ranks)
-lin_tax_tab.to_csv(snakemake.output[0],sep='\t')
-
-
-# tb=lin_tax_tab[['last_taxid','last_rank','taxid_path','read_counts']]
-# tb.columns=['TAXID','RANK','TAXPATH','COUNTS']
-# tb_human_filtered=tb.drop([9606])#drop human reads
-# tb_human_filtered['PERCENTAGE']=tb_human_filtered['COUNTS'].div(sum(tb_human_filtered['COUNTS'])).mul(100)
-# tb_human_filtered=tb_human_filtered[['TAXID','RANK','TAXPATH','PERCENTAGE']]
-#
-# row_list=[]
-# for index, row in tb_human_filtered.iterrows():
-#     taxid=row['TAXID']
-#     rank=row['RANK']
-#     path=row['TAXPATH']
-#     percent=float(row['PERCENTAGE'])
-#     row_list.append(f"{taxid}\t{rank}\t{path}\t{percent}")
-#
-# str_tb='\n'.join(row_list)
-#
-# profile_file=open(snakemake.output[0],"w+")
-# profile_file.write(f'@SampleID:{sample}\n\
-# @Version:0.9.1"\n\
-# @Ranks:superkingdom|phylum|order|family|genus|species\n\
-# @TaxonomyID:ncbi-taxonomy_{d}\n\
-# @@TAXID\tRANK\tTAXPATH\tPERCENTAGE\n\
-# {str_tb}')
-# profile_file.close()
+lin_tax_tab.to_csv(snakemake.output[0],sep='\t',index=None)
