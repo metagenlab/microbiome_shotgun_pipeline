8a3d965aaacdefa3e0b3f17b4bdf9afc6197eb66
idfarbanecha <faridchaabane23@gmail.com>
2019-11-01 16:49:38 +0100

    adjusted fig size and margins

--- rules/compare_tools/scripts/heatmap-presence.py
+++ rules/compare_tools/scripts/heatmap-presence.py
@@ -8,10 +8,11 @@ tb=tb.replace(True,1)
 subset=tb[[f'{rank}','tool','present']]
 matrix=pd.pivot_table(data=subset,index=subset['species'],columns=subset['tool'])
 matrix.columns=matrix.columns.droplevel()
-plt.figure(figsize=(20,10))
-sns.set(font_scale=1.2)
+plt.figure(figsize=(11.7,8.27))
+sns.set(font_scale=1.0)
 myColors = ((0.0, 0.0, 0.0, 0.9), (0.3, 0.6, 0.1, 1.0))#RGB color palette
 cmap = LinearSegmentedColormap.from_list('Custom', myColors, len(myColors))
+plt.subplots_adjust(left=0.2,bottom=0.2)
 ax=sns.heatmap(matrix,linecolor='black',linewidths='0.1',cmap=cmap,square=True)
 colorbar = ax.collections[0].colorbar
 colorbar.set_ticks([0,1.0])
