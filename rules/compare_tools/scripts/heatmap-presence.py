848c4db16b8786fee25fcba7e8da124d17ff3ac4
idfarbanecha <faridchaabane23@gmail.com>
2019-11-04 19:12:03 +0100

    corrected colouring when only 1 value is present

--- rules/compare_tools/scripts/heatmap-presence.py
+++ rules/compare_tools/scripts/heatmap-presence.py
@@ -6,11 +6,15 @@ rank=snakemake.params["rank"]
 tb=pd.read_csv(snakemake.input[0],sep='\t')
 tb=tb.replace(True,1)
 subset=tb[[f'{rank}','tool','present']]
-matrix=pd.pivot_table(data=subset,index=subset['species'],columns=subset['tool'])
+matrix=pd.pivot_table(data=subset,index=subset[f'{rank}'],columns=subset['tool'])
 matrix.columns=matrix.columns.droplevel()
 plt.figure(figsize=(11.7,8.27))
 sns.set(font_scale=1.0)
-myColors = ((0.0, 0.0, 0.0, 0.9), (0.3, 0.6, 0.1, 1.0))#RGB color palette
+catgeories=list(set(subset['present']))
+if len(catgeories)==1 and catgeories[0]==1:
+    myColors = [(0.3, 0.6, 0.1, 1.0),(0.0, 0.0, 0.0, 0.9)]
+else:
+    myColors = [(0.0, 0.0, 0.0, 0.9), (0.3, 0.6, 0.1, 1.0)]#RGB color palette
 cmap = LinearSegmentedColormap.from_list('Custom', myColors, len(myColors))
 plt.subplots_adjust(left=0.2,bottom=0.2)
 ax=sns.heatmap(matrix,linecolor='black',linewidths='0.1',cmap=cmap,square=True)
