f7aeab0765a0cbb8c83ca96b311c9dda54ada247
idfarbanecha <faridchaabane23@gmail.com>
2019-11-22 17:55:31 +0100

    adapted color for when there are no false negatives

--- rules/compare_tools/scripts/heatmap-presence.py
+++ rules/compare_tools/scripts/heatmap-presence.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
+
 rank=snakemake.params["rank"]
 tb=pd.read_csv(snakemake.input[0],sep='\t')
-tb=tb.replace(True,1)
+tb=tb.replace({"TP":1,"FP":2,"FN":0})
 subset=tb[[f'{rank}','tool','present']]
-matrix=pd.pivot_table(data=subset,index=subset[f'{rank}'],columns=subset['tool'])
-matrix.columns=matrix.columns.droplevel()
-plt.figure(figsize=(11.7,8.27))
-sns.set(font_scale=1.0)
-categories=list(set(subset['present']))
-if len(categories)==1 and categories[0]==1:
-    myColors = [(0.3, 0.6, 0.1, 1.0),(0.0, 0.0, 0.0, 0.9)]
+
+matrix=pd.pivot(data=subset,index=f'{rank}',columns='tool',values='present')
+matrix=matrix.replace(np.nan,3)
+col_list=list(matrix.columns)
+matrix=matrix.sort_values(by=col_list,axis=0,ascending=True)
+if 0 not in np.unique(matrix):
+    myColors = [(0.3, 0.6, 0.1, 1.0), (1.0, 0.64, 0, 1.0), (0, 0, 0, 0)]  # Black green orange white
 else:
-    myColors = [(0.0, 0.0, 0.0, 0.9), (0.3, 0.6, 0.1, 1.0)]#RGB color palette
+    myColors = [(0.0, 0.0, 0.0, 0.9), (0.3, 0.6, 0.1, 1.0), (1.0, 0.64, 0, 1.0),
+                (0, 0, 0, 0)]  # Black green orange white
+
 cmap = LinearSegmentedColormap.from_list('Custom', myColors, len(myColors))
-plt.subplots_adjust(left=0.2,bottom=0.2)
-ax=sns.heatmap(matrix,linecolor='black',linewidths='0.1',cmap=cmap,square=True)
+
+plt.figure(figsize=(11.7,8.27))
+sns.set(font_scale=0.6)
+plt.subplots_adjust(left=0.8)
+ax=sns.heatmap(matrix,linecolor='black',linewidths='0.1',cmap=cmap,cbar_kws={'fraction' : 0.04},square=True)
 colorbar = ax.collections[0].colorbar
-colorbar.set_ticks([0,1.0])
-colorbar.set_ticklabels(['absent', 'present'])
+colorbar.set_ticks([0,1.0,2.0,3.0])
+colorbar.set_ticklabels(["FN","TP","FP","NA"])
 ax.get_figure().savefig(snakemake.output[0])
