a1d26c1f34aa1e2e5618da4822dbe9f572fc3032
idfarbanecha <faridchaabane23@gmail.com>
2019-11-22 17:56:22 +0100

    adapted code to support read thresholds

--- rules/compare_tools/scripts/stats.py
+++ rules/compare_tools/scripts/stats.py
@@ -4,16 +4,16 @@ import pandas as pd
 rank=snakemake.config["target_rank"]
 tool_name=snakemake.wildcards.tool
 
+bac_threshold=snakemake.params.bac_threshold
+vir_threshold=snakemake.params.vir_threshold
 
-def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name, superkingdom):
-    tool_tb = tool_tb.replace(np.nan, 'NA')
-    true_tb = true_tb.replace(np.nan, 'NA')
-
-    tool_tb = tool_tb[tool_tb.superkingdom == superkingdom]
-    true_tb = true_tb[true_tb.superkingdom == superkingdom]
-    true_list = list(true_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
-    tool_list = list(tool_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
-
+def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name):
+    if rank=='scientific_name':
+        taxid_col='taxid'
+    else:
+        taxid_col=f'{rank}_taxid'
+    true_list = list(true_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
+    tool_list = list(tool_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
     tp_list = [i for i in true_list if i in true_list and i in tool_list]
     fp_list = [i for i in tool_list if i not in true_list and i in tool_list]
     fn_list = [i for i in true_list if i in true_list and i not in tool_list]
@@ -36,26 +36,33 @@ def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name, superkingdom):
 
 
 
-def get_presence(true_tb,tool_tb,rank,tool_name,superkingdom):
-    tool_tb = tool_tb.replace(np.nan, 'NA')
-    true_tb = true_tb.replace(np.nan, 'NA')
+def get_presence(true_tb,tool_tb,rank,tool_name):
 
-    tool_tb = tool_tb[tool_tb.superkingdom == superkingdom]
-    true_tb = true_tb[true_tb.superkingdom == superkingdom]
-    names = list(true_tb[f'{rank}'])
-    taxids = list(true_tb[f'{rank}_taxid'])
-    taxid2names = dict(zip(taxids, names))
-    true_list = list(true_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
-    tool_list = list(tool_tb.groupby([f'{rank}_taxid'], as_index=False).sum()[f'{rank}_taxid'])
+    if rank=='scientific_name':
+        taxid_col='taxid'
+    else:
+        taxid_col=f'{rank}_taxid'
+
+    true_names = list(true_tb[f'{rank}'])
+    true_taxids = list(true_tb[taxid_col])
+    true_taxid_names = dict(zip(true_taxids, true_names))
+    tool_taxids=list(tool_tb[taxid_col])
+    tool_names=list(tool_tb[f'{rank}'])
+    tool_taxid_names=dict(zip(tool_taxids,tool_names))
+    true_list = list(true_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
+    tool_list = list(tool_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
     tp_list = [i for i in true_list if i in true_list and i in tool_list]
     fn_list = [i for i in true_list if i in true_list and i not in tool_list]
+    fp_list = [i for i in tool_list if i not in true_list and i in tool_list]
     dic={}
-    for taxid in taxids:
+    for taxid in true_taxid_names:
         dic[taxid]={}
         if taxid in tp_list:
-            dic[taxid]={'tool':tool_name,'taxid':str(taxid),f'{rank}':taxid2names[taxid],'present':True}
+            dic[taxid]={'tool':tool_name,'taxid':str(taxid),f'{rank}':true_taxid_names[taxid],'present':"TP"}
         if taxid in fn_list:
-            dic[taxid]={'tool':tool_name,'taxid':str(taxid),f'{rank}':taxid2names[taxid],'present':False}
+            dic[taxid]={'tool':tool_name,'taxid':str(taxid),f'{rank}':true_taxid_names[taxid],'present':"FN"}
+    for taxid in fp_list:
+            dic[taxid]={'tool':tool_name,'taxid':str(taxid),f'{rank}':tool_taxid_names[taxid],'present':"FP"}
 
     df=pd.DataFrame.from_dict(dic, orient='index')
     return df
@@ -63,11 +70,23 @@ def get_presence(true_tb,tool_tb,rank,tool_name,superkingdom):
 gold_standard=pd.read_csv(snakemake.input.gold_standard,sep='\t')
 tool_output=pd.read_csv(snakemake.input.tool_out,sep='\t')
 
-virus_scores=get_precision_recall_f1(gold_standard,tool_output,rank,tool_name,'Viruses')
-virus_presence=get_presence(gold_standard,tool_output,rank,tool_name,'Viruses')
+true_vir=gold_standard[gold_standard.superkingdom=='Viruses']
+true_bac=gold_standard[gold_standard.superkingdom=='Bacteria']
+
+tool_vir=tool_output[tool_output.superkingdom=='Viruses']
+tool_vir=tool_vir[tool_vir.read_counts>vir_threshold]
+tool_vir=tool_vir.replace(np.nan,'NA')
+tool_bac=tool_output[tool_output.superkingdom=='Bacteria']
+tool_bac=tool_bac[tool_bac.read_counts>bac_threshold]
+tool_bac=tool_bac.replace(np.nan,'NA')
+
+gold_standard = gold_standard.replace(np.nan, 'NA')
+
+virus_scores=get_precision_recall_f1(true_vir,tool_vir,rank,tool_name)
+virus_presence=get_presence(true_vir,tool_vir,rank,tool_name)
 virus_scores.to_csv(snakemake.output.vir_scores,sep='\t',index=None)
 virus_presence.to_csv(snakemake.output.vir_presence,sep='\t',index=None)
-bacteria_scores=get_precision_recall_f1(gold_standard,tool_output,rank,tool_name,'Bacteria')
-bacteria_presence=get_presence(gold_standard,tool_output,rank,tool_name,'Bacteria')
+bacteria_scores=get_precision_recall_f1(true_bac,tool_bac,rank,tool_name)
+bacteria_presence=get_presence(true_bac,tool_bac,rank,tool_name)
 bacteria_presence.to_csv(snakemake.output.bac_presence,sep='\t',index=None)
 bacteria_scores.to_csv(snakemake.output.bac_scores,sep='\t',index=None)
