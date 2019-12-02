8603543ca586ff6d7b450daaea03ba10b2d28dcc
idfarbanecha <faridchaabane23@gmail.com>
2019-12-02 17:43:51 +0100

    adapted script to virus and bacteria seperation

--- rules/compare_tools/scripts/concat.py
+++ rules/compare_tools/scripts/concat.py
@@ -11,12 +11,8 @@ def concat_tables(file_list):
         all_tools=all_tools.sort_values(by='F1_score',ascending=False)
     return all_tools
 
-vir_scores=concat_tables(snakemake.input.vir_scores)
-vir_scores.to_csv(snakemake.output.all_vir_scores,sep='\t')
-vir_presence=concat_tables(snakemake.input.vir_presence)
-vir_presence.to_csv(snakemake.output.all_vir_presence,sep='\t')
-bac_scores=concat_tables(snakemake.input.bac_scores)
-bac_scores.to_csv(snakemake.output.all_bac_scores,sep='\t')
-bac_presence=concat_tables(snakemake.input.bac_presence)
-bac_presence.to_csv(snakemake.output.all_bac_presence,sep='\t')
+scores=concat_tables(snakemake.input.scores)
+scores.to_csv(snakemake.output.all_scores,sep='\t')
+presence=concat_tables(snakemake.input.presence)
+presence.to_csv(snakemake.output.all_presence,sep='\t')
 
