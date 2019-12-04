1fac9791b880c1b152c7ab2f0cd064e650767f40
idfarbanecha <faridchaabane23@gmail.com>
2019-12-04 17:19:40 +0100

    corrected linear taxid function

--- rules/compare_tools/scripts/ganon-merge.py
+++ rules/compare_tools/scripts/ganon-merge.py
@@ -14,60 +14,42 @@ for path in input_file_list:
 
 def get_lin_tax(tab, ncbi, target_ranks):
     tax = {}
-    tab=tab.replace('unclassified',np.nan)
     tab=tab.replace(np.nan,0)
+    tab=tab.replace('unclassified',0)
     taxid_list = tab['taxid']
     percents = tab['read_percent']
     counts = tab['reads_assigned']
     count_dic = dict(zip(taxid_list, counts))
     percent_dic = dict(zip(taxid_list, percents))
     for taxid in taxid_list:
-        tax[taxid] = {'taxid': str(taxid), 'read_percent': percent_dic[taxid], 'read_counts': count_dic[taxid]}
+        tax[taxid] = {'taxid':str(taxid), 'read_percent': percent_dic[taxid], 'read_counts': count_dic[taxid]}
         if int(taxid) > 0:
-            try:
-                scientific_name = ncbi.translate_to_names([taxid])[0]
-                if int(scientific_name)==True:
-                    scientific_name = list(ncbi.get_taxid_translator([taxid]).values())
-                    if len(scientific_name)==0:
-                        tax[taxid]['scientific_name'] = 'NA'
-                    else:
-                        tax[taxid]['scientific_name'] = str(scientific_name[0])
-                lineage = ncbi.get_lineage(taxid)
-                names = ncbi.get_taxid_translator(lineage)
-                ranks = ncbi.get_rank(lineage)
-                rank_list = list(ranks.values())
-                name_list = list(names.values())
-                rank2names = dict(zip(rank_list, name_list))
-                for sub_taxid in lineage:
-                    rank = ranks[sub_taxid]
-                    if rank in target_ranks:
-                        tax[taxid][f"{rank}_taxid"] = sub_taxid
-                for n, rank in enumerate(target_ranks):
-                    if rank in rank2names.keys():
-                        tax[taxid][rank] = rank2names[rank]
-                    else:
-                        previous_name = 'root'
-                        for ranks_to_skip in range(1, len(target_ranks) + 1):
-                            previous_rank = target_ranks[n - ranks_to_skip]
-                            if previous_rank in rank2names.keys():
-                                previous_name = rank2names[previous_rank]
-                                break
-                            if previous_rank not in rank2names.keys():
-                                continue
-                        tax[taxid][rank] = f'{previous_name}_{rank[0:1]}'
-            except ValueError:
-                tax[taxid]['superkingdom']='NA'
-                tax[taxid]['superkingdom_taxid']='NA'
-                tax[taxid]['phylum'] = 'NA'
-                tax[taxid]['phylum_taxid']='NA'
-                tax[taxid]['order'] = 'NA'
-                tax[taxid]['order_taxid'] = 'NA'
-                tax[taxid]['family'] = 'NA'
-                tax[taxid]['family_taxid'] = 'NA'
-                tax[taxid]['genus'] = 'NA'
-                tax[taxid]['genus_taxid'] = 'NA'
-                tax[taxid]['species'] = 'NA'
-                tax[taxid]['species_taxid'] = 'NA'
+            scientific_name = ncbi.translate_to_names([taxid])[0]
+            tax[taxid]['scientific_name']=scientific_name
+            lineage = ncbi.get_lineage(taxid)
+            names = ncbi.get_taxid_translator(lineage)
+            ranks = ncbi.get_rank(lineage)
+            rank_list = list(ranks.values())
+            name_list = list(names.values())
+            rank2names = dict(zip(rank_list, name_list))
+
+            for sub_taxid in lineage:
+                rank = ranks[sub_taxid]
+                if rank in target_ranks:
+                    tax[taxid][f"{rank}_taxid"] = sub_taxid
+            for n, rank in enumerate(target_ranks):
+                if rank in rank2names.keys():
+                    tax[taxid][rank] = rank2names[rank]
+                else:
+                    previous_name = 'root'
+                    for ranks_to_skip in range(1, len(target_ranks) + 1):
+                        previous_rank = target_ranks[n - ranks_to_skip]
+                        if previous_rank in rank2names.keys():
+                            previous_name = rank2names[previous_rank]
+                            break
+                        if previous_rank not in rank2names.keys():
+                            continue
+                    tax[taxid][rank] = f'{previous_name}_{rank[0:1]}'
     df = pd.DataFrame.from_dict(tax, orient='index')
     df = df.replace(np.nan, 'NA')
     return df
