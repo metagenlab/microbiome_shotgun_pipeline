3b10ae1d9b30a3bd37e0e84c7fb8457955d314dd
tpillone <trestan.pillonel@gmail.com>
2019-12-13 16:55:06 +0100

    custer db with silix, add silix data to final rgi tables, update rgi versions

--- rules/annotation/scripts/merge_rgi.py
+++ rules/annotation/scripts/merge_rgi.py
@@ -1,6 +1,55 @@
 import pandas
 
-rgi_files = snakemake.input
+rgi_files = snakemake.input["rgi_output"]
+card_annotation = snakemake.input["card_annotation"]
+silix_98 = snakemake.input["silix_98"]
+silix_95 = snakemake.input["silix_95"]
+silix_90 = snakemake.input["silix_90"]
+silix_80 = snakemake.input["silix_80"]
+silix_98_annotation = snakemake.input["silix_98_annotation"]
+silix_95_annotation = snakemake.input["silix_95_annotation"]
+silix_90_annotation = snakemake.input["silix_90_annotation"]
+silix_80_annotation = snakemake.input["silix_80_annotation"]
+
+
+accession2silix_98 = pandas.read_csv(silix_98,
+                                     delimiter='\t',
+                                     names=["silix_name","accession"]).set_index("accession").to_dict()["silix_name"]
+
+accession2silix_95 = pandas.read_csv(silix_95,
+                                     delimiter='\t',
+                                     names=["silix_name","accession"]).set_index("accession").to_dict()["silix_name"]
+
+accession2silix_90 = pandas.read_csv(silix_90,
+                                     delimiter='\t',
+                                     names=["silix_name","accession"]).set_index("accession").to_dict()["silix_name"]
+
+accession2silix_80 = pandas.read_csv(silix_80,
+                                     delimiter='\t',
+                                     names=["silix_name","accession"]).set_index("accession").to_dict()["silix_name"]
+
+silix_98_2_annotation = pandas.read_csv(silix_98_annotation,
+                                     delimiter='\t',
+                                     names=["silix_name","description"]).set_index("silix_name").to_dict()["description"]
+
+silix_95_2_annotation = pandas.read_csv(silix_95_annotation,
+                                     delimiter='\t',
+                                     names=["silix_name","description"]).set_index("silix_name").to_dict()["description"]
+
+silix_90_2_annotation = pandas.read_csv(silix_90_annotation,
+                                     delimiter='\t',
+                                     names=["silix_name","description"]).set_index("silix_name").to_dict()["description"]
+
+silix_80_2_annotation = pandas.read_csv(silix_80_annotation,
+                                     delimiter='\t',
+                                     names=["silix_name","description"]).set_index("silix_name").to_dict()["description"]
+
+# ACT97415.1	ACT97415	ARO:3002999	CblA beta-lactamase
+aro2accession = pandas.read_csv(card_annotation,
+                                delimiter='\t',
+                                names=["accession","accession_no_version", "ARO", "description"]).set_index("ARO").to_dict()["accession"]
+
+
 sample_table = snakemake.params[0]
 
 all_samples = pandas.read_csv(sample_table, sep="\t", index_col=0)
@@ -62,6 +111,7 @@ header1 = ["tool",
            "group_1",
            "group_2", 
            "contig_id",
+           "contig_id_uniq"
            "ORF_id",
            "cut_Off",
            "pass_bitscore",
@@ -70,7 +120,16 @@ header1 = ["tool",
            "best_identities",
            "ARO",
            "model_type",
-           "percentage_length"]
+           "percentage_length",
+           "accession",
+           "silix_98",
+           "silix_98_decription",
+           "silix_95",
+           "silix_95_decription",
+           "silix_90",
+           "silix_90_decription",
+           "silix_80",
+           "silix_80_decription"]
 
 header2 = ["aro", "drug_class"]
 header3 = ["aro", "resistance_mechanism"]
@@ -80,19 +139,6 @@ data = []
 
 o_rgi_hits = open(snakemake.output[0], "w")
 o_rgi_hits.write("\t".join(header1) + '\n')
-
-o_aro2mechanism = open(snakemake.output[1], "w")
-o_aro2mechanism.write("\t".join(header2) + '\n')
-
-o_aro2gene_family = open(snakemake.output[2], "w")
-o_aro2gene_family.write("\t".join(header3) + '\n')
-
-o_aro2drug_class = open(snakemake.output[3], "w")
-o_aro2drug_class.write("\t".join(header4) + '\n')
-
-aro2mechanism = {}
-aro2gene_family = {}
-aro2drug_class = {}
 
 for rgi_file in rgi_files:
     sample_name = rgi_file.split("/")[1]
@@ -105,17 +151,28 @@ for rgi_file in rgi_files:
             else:
                 
                 data = row.rstrip().split("\t")
+
                 if "plass" in rgi_file:
-                    ## M03935:66:000000000-B656J:1:1101:14307:380 [Orf: 5677, 0, 135, -1, 1, 0]
+                    ## M03935:66:000000000-B656J:1:1101:14307:380 [Orf: 5677, 0, 135, -1, 1, 0] => renamed ORF_X
                     tool = "plass"
                     contig_id = None
-                    orf_number = data[0].split("[Orf: ")[1].split(",")[0]
-                elif "prodigal" in  rgi_file:
+                    unique_id = None
+                    orf_number = data[0] # .split("[Orf: ")[1].split(",")[0]
+                if "prodigal" in  rgi_file:
                     # ok 0 ORF_ID NODE_270_1 # 57 # 1982 # 1 # ID=270_1;partial=00;start_type=ATG;rbs_motif=None;rbs_spacer=None;gc_cont=0.398
                     tool = "prodigal"
                     contig_prefix, contig_number, orf_number = data[0].split(" ")[0].split("_")
                     contig_id = "%s_%s" % (contig_prefix, contig_number)
-
+                    contig_uniq = "%s_%s" % (sample_name, contig_id)
+                if "contig" in rgi_file:
+                    tool = "contigs"
+                    if not "NODE_" in data[0]:
+                        print("Skipping non protein homolog model data:", data[0])
+                        continue
+                    contig_prefix, contig_number, orf_number = data[0].split(" ")[0].split("_")
+                    contig_id = "%s_%s" % (contig_prefix, contig_number)
+                    contig_uniq = "%s_%s" % (sample_name, contig_id)
+                                   
                 cut_off = data[5]
                 pass_bitscore = data[6]
                 best_hit_bitscore = data[7]
@@ -124,37 +181,51 @@ for rgi_file in rgi_files:
                 ARO = "ARO:%s" % data[10]
                 model_type = data[11]
                 percentage_length = data[20]
+            
+                if model_type != 'protein homolog model':
+                    continue
 
-                o_rgi_hits.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (tool,
-                                                                                           sample_name,
-                                                                                           group_1,
-                                                                                           group_2,
-                                                                                           contig_id,
-                                                                                           orf_number,
-                                                                                           cut_off,
-                                                                                           pass_bitscore,
-                                                                                           best_hit_bitscore,
-                                                                                           best_hit_ARO,
-                                                                                           best_identities,
-                                                                                           ARO,
-                                                                                           model_type,
-                                                                                           percentage_length
+                try:
+                    accession = aro2accession[ARO]
+                except KeyError:
+                    print(data)
+                    continue
+                silix_98 = accession2silix_98[accession]
+                silix_98_description = silix_98_2_annotation[silix_98]
+
+                silix_95 = accession2silix_95[accession]
+                silix_95_description = silix_95_2_annotation[silix_95]
+
+                silix_90 = accession2silix_90[accession]
+                silix_90_description = silix_90_2_annotation[silix_90]
+
+                silix_80 = accession2silix_80[accession]
+                silix_80_description = silix_80_2_annotation[silix_80]
+
+
+                o_rgi_hits.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (tool,
+                                                                                              sample_name,
+                                                                                               group_1,
+                                                                                                group_2,
+                                                                                                contig_id,
+                                                                                                contig_uniq,
+                                                                                                orf_number,
+                                                                                                cut_off,
+                                                                                                pass_bitscore,
+                                                                                                best_hit_bitscore,
+                                                                                                best_hit_ARO,
+                                                                                                best_identities,
+                                                                                                ARO,
+                                                                                                model_type,
+                                                                                                percentage_length,
+                                                                                                accession,
+                                                                                                silix_98,
+                                                                                                silix_98_description,
+                                                                                                silix_95,
+                                                                                                silix_95_description,
+                                                                                                silix_90,
+                                                                                                silix_90_description,
+                                                                                                silix_80,
+                                                                                                silix_80_description
                                                                                            ))
 
-                if ARO not in aro2mechanism:
-                    drug_class_list = data[14].split(";")
-                    mechanism_list = data[15].split(";")
-                    gene_family_list = data[16].split(";")
-
-                    aro2mechanism[ARO] = mechanism_list
-                    aro2gene_family[ARO] = gene_family_list
-                    aro2drug_class[ARO] = drug_class_list
-for aro in aro2mechanism:
-    for mechanism in aro2mechanism[aro]:
-        o_aro2mechanism.write("%s\t%s\n" % (aro, mechanism))
-
-    for family in aro2gene_family[aro]:
-        o_aro2gene_family.write("%s\t%s\n" % (aro, family))   
-
-    for drug_class in aro2drug_class[aro]:
-        o_aro2drug_class.write("%s\t%s\n" % (aro, drug_class))
--- rules/databases/resistance/scripts/aro2data.py
+++ rules/databases/resistance/scripts/aro2data.py
@@ -1,34 +1,69 @@
+from Bio import SeqIO 
+
 o = open(snakemake.output[0], "w") # accession2AMR_family.tab
 p = open(snakemake.output[1], "w") # accession2drug_class.tab
 q = open(snakemake.output[2], "w") # accession2resistance_mechanism.tab
 
 aro_index = snakemake.input[0]
 aro_categories_index = snakemake.input[1]
+CARD_homolog_model_fasta = snakemake.input[2]
 
-protein_accession2ARO = {}
-with open(aro_index, 'r') as f:
-    for n, row in enumerate(f):
-        if n == 0:
-            continue
-        else:
-            data = row.rstrip().split("\t")
-            protein_accession2ARO[data[6]] = data[0]
 
-with open(aro_categories_index, 'r') as f:
-    for	n, row in enumerate(f):
-        if n ==	0:
-            continue
-        else:
-            data = row.rstrip().split("\t")
-            protein_accession = data[0]
-            protein_accession_no_version = data[0].split(".")[0]
-            ARO = protein_accession2ARO[protein_accession]
-            AMR_family_list = data[2].split(";")
-            for family in AMR_family_list:
-                o.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, family))
-            drug_class_list = data[3].split(";")
-            for drug_class in drug_class_list:
-                p.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, drug_class))
-            resistance_mechanism_list = data[4].split(";")
-            for mechanism in resistance_mechanism_list:
-                q.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, mechanism))
+def get_accession2aro(aro_index):
+
+    protein_accession2ARO = {}
+    with open(aro_index, 'r') as f:
+        for n, row in enumerate(f):
+            if n == 0:
+                continue
+            else:
+                data = row.rstrip().split("\t")
+                protein_accession2ARO[data[6]] = data[0]
+    return protein_accession2ARO
+
+
+def get_aro2category_data(aro_categories_index, protein_accession2ARO):
+    aro2category_data = {}
+    with open(aro_categories_index, 'r') as f:
+        for	n, row in enumerate(f):
+            if n ==	0:
+                continue
+            else:
+                data = row.rstrip().split("\t")
+                protein_accession = data[0]
+                if protein_accession == 'N/A':
+                    print("Skipping mutation conferring resistance:", data[2])
+                    continue
+                ARO = protein_accession2ARO[protein_accession]
+                aro2category_data[ARO] = {}
+
+                AMR_family_list = data[2].split(";")
+                drug_class_list = data[3].split(";")
+                resistance_mechanism_list = data[4].split(";")
+
+                aro2category_data[ARO]["AMR_family_list"] = AMR_family_list
+                aro2category_data[ARO]["drug_class_list"] = drug_class_list
+                aro2category_data[ARO]["resistance_mechanism_list"] = resistance_mechanism_list
+    return aro2category_data
+
+
+accession2aro = get_accession2aro(aro_index)
+aro2category_data = get_aro2category_data(aro_categories_index, accession2aro)
+
+records = SeqIO.parse(CARD_homolog_model_fasta, 'fasta')
+
+for record in records:
+    protein_accession = record.id
+    protein_accession_no_version = protein_accession.split(".")[0]
+    ARO = accession2aro[protein_accession]
+    
+    AMR_family_list = aro2category_data[ARO]["AMR_family_list"]
+    drug_class_list = aro2category_data[ARO]["drug_class_list"]
+    resistance_mechanism_list = aro2category_data[ARO]["resistance_mechanism_list"]
+
+    for family in AMR_family_list:
+        o.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, family))
+    for drug_class in drug_class_list:
+        p.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, drug_class))
+    for mechanism in resistance_mechanism_list:
+        q.write("%s\t%s\t%s\t%s\n" % (protein_accession, protein_accession_no_version, ARO, mechanism))
