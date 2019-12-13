18f5b36a5fd958c7a58b2ede984f8cf8f46db48c
tpillone <trestan.pillonel@gmail.com>
2019-12-13 20:00:55 +0100

    typo

--- rules/protein_abundance/scripts/resistance_annotation.py
+++ rules/protein_abundance/scripts/resistance_annotation.py
@@ -9,7 +9,7 @@ import pandas
 
 o = open(snakemake.output[0], 'w')
 
-RPKM_table = snakemake.input["RPKM_table]
+RPKM_table = snakemake.input["RPKM_table"]
 
 card_annotation = snakemake.input["card_annotation"]
 silix_98 = snakemake.input["silix_98"]
@@ -65,7 +65,7 @@ with open(RPKM_table, 'r') as f:
         if n == 0:
             # header 
             # sample sequence_accession n_hits RPKM group_1 group_2
-            o.write(row.rstrip()+='\tARO\tsilix_98\tsilix_98_description\tsilix_95\tsilix_95_description\tsilix_90\tsilix_90_description\tsilix_80\tsilix_80_description\n')
+            o.write(row.rstrip() + '\tARO\tsilix_98\tsilix_98_description\tsilix_95\tsilix_95_description\tsilix_90\tsilix_90_description\tsilix_80\tsilix_80_description\n')
         else:
             accession = data[1]
             aro = accession2aro[accession]
@@ -81,5 +81,5 @@ with open(RPKM_table, 'r') as f:
 
             silix_80 = accession2silix_80[accession]
             silix_80_description = silix_80_2_annotation[silix_80]
-            o.write(row.rstrip()+='\t{aro}\t{silix_98}\t{silix_98_description}\t{silix_95}\t{silix_95_description}\t{silix_90}\t{silix_90_description}\t{silix_80}\t{silix_80_description}\n')
+            o.write(row.rstrip()+ f'\t{aro}\t{silix_98}\t{silix_98_description}\t{silix_95}\t{silix_95_description}\t{silix_90}\t{silix_90_description}\t{silix_80}\t{silix_80_description}\n')
 
