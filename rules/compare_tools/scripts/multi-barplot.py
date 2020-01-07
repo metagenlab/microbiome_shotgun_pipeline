8a4661b9f2be917598f79250162da00d36b17a3a
idfarbanecha <faridchaabane23@gmail.com>
2020-01-07 19:55:35 +0100

    corrected read counts for ezvir

--- rules/compare_tools/scripts/multi-barplot.py
+++ rules/compare_tools/scripts/multi-barplot.py
@@ -8,25 +8,19 @@ rank=snakemake.params.rank
 gold_standard = pd.read_csv(snakemake.input.gold_standard,sep='\t')
 superkingdom_gs=gold_standard[gold_standard.superkingdom==superkingdom]
 
-superkingdom_gs=superkingdom_gs.groupby(f'{rank}',as_index=False).sum()
-superkingdom_gs['tool']=['gold_standard']*len(superkingdom_gs)
-subset_gs=superkingdom_gs[[f'{rank}','read_counts','tool']]
 
 
 
 def groupby_samples(table,tool_name,rank):
-    samples=sorted(list(set(table['sample'])))
-    tables=[]
+    if tool_name=='ezvir':
+        sample_tb=table.groupby(['sample',f'{rank}'],as_index=False)['read_counts'].sum()
+        sample_tb['read_counts']=sample_tb['read_counts']/2
+    else:
+        sample_tb = table.groupby(['sample', f'{rank}'], as_index=False)['read_counts'].sum()
+    sample_tb['tool'] = [tool_name] * len(sample_tb)
+    return sample_tb
 
-    for sample in samples:
-        sample_tb = table[table['sample'] == sample]
-        sample_tb = sample_tb.groupby(f'{rank}', as_index=False).sum()
-        sample_tb['tool'] = [tool_name] * len(sample_tb)
-        subset=sample_tb[[f'{rank}','read_counts','tool']]
-        tables.append(subset)
-    all_samples=pd.concat(tables,sort=False)
-    return all_samples
-
+subset_gs=groupby_samples(superkingdom_gs,'gold_standard',rank)
 
 files=snakemake.input.tool_out
 tables=[]
@@ -42,8 +36,9 @@ all_tools=pd.concat(tables,sort=False)
 
 gs_tool_tb=pd.concat([subset_gs,all_tools],sort=False)
 
+
 gs_tool_tb.set_index(f'{rank}',inplace=True)
-true_genomes_list=list(gs_tool_tb.loc[gs_tool_tb['tool']=='gold_standard'].index)
+true_genomes_list=list(set(gs_tool_tb.loc[gs_tool_tb['tool']=='gold_standard'].index))
 gs_tool_tb.reset_index(inplace=True)
 true_pos=gs_tool_tb.loc[gs_tool_tb[f'{rank}'].isin(true_genomes_list)]
 true_pos=true_pos.sort_values(by='read_counts',ascending=False)
