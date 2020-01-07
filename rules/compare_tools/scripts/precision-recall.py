1359c67ad4daef7e5c4a59b59587f9ce0c910ec0
idfarbanecha <faridchaabane23@gmail.com>
2020-01-07 19:57:34 +0100

    optimized false positive computation

--- rules/compare_tools/scripts/precision-recall.py
+++ rules/compare_tools/scripts/precision-recall.py
@@ -7,16 +7,18 @@ rank=snakemake.params.rank
 superkingdom=snakemake.params.superkingdom
 gold_standard_file=snakemake.input.gold_standard
 read_step=snakemake.params.read_step
-
+max_threshold=snakemake.params.max_read
 
 gold_standard=pd.read_csv(gold_standard_file,sep='\t')
 true_superkingdom=gold_standard[gold_standard['superkingdom']==superkingdom]
-max_threshold=max(true_superkingdom.read_counts)
+true_superkingdom.insert(loc=true_superkingdom.shape[1],column='tool',value=['gold_standard']*len(true_superkingdom))
 
 
 tb_list=[]
+tool_list=[]
 for file in tables:
     tool = file.split('/')[1]
+    tool_list.append(tool)
     tb=pd.read_csv(file,sep='\t')
     tb['tool']=[tool]*len(tb)
     tb_list.append(tb)
@@ -26,19 +28,21 @@ tools_superkingdom=all_tools[all_tools.superkingdom==superkingdom]
 
 
 
-def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name):
-    if rank=='scientific_name':
-        taxid_col='taxid'
-    else:
-        taxid_col=f'{rank}_taxid'
-    true_list = list(true_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
-    tool_list = list(tool_tb.groupby([taxid_col], as_index=False).sum()[taxid_col])
-    tp_list = [i for i in true_list if i in true_list and i in tool_list]
-    fp_list = [i for i in tool_list if i not in true_list and i in tool_list]
-    fn_list = [i for i in true_list if i in true_list and i not in tool_list]
-    tp = len(tp_list)
-    fp = len(fp_list)
-    fn = len(fn_list)
+def get_precision_recall_f1(true_tb, tool_tb, rank,tool_name,threshold):
+    tool_tb=tool_tb[tool_tb['read_counts']>=threshold]
+    true_list = list(set(true_tb[f'{rank}_taxid']))
+    tool_list = list(set(tool_tb[f'{rank}_taxid']))
+    tp=0
+    fn=0
+    for i in true_list:
+        if tp<len(true_list) and i in true_list and i in tool_list:
+            tp+=1
+        if tp<len(true_list) and i in true_list and i not in tool_list:
+            fn+=1
+        if tp+fn==len(true_list):
+            break
+
+    fp = len(tool_list)-tp
     try:
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
@@ -49,43 +53,32 @@ def get_precision_recall_f1(true_tb, tool_tb, rank, tool_name):
         f1 = 0
     scores = {}
     scores[tool_name] = {'tool':tool_name,'tp': tp, 'fp': fp, 'fn': fn, 'precision': precision, 'recall': recall,
-                            'F1_score': f1}
+                            'F1_score': f1,'threshold':threshold}
     df = pd.DataFrame.from_dict(scores, orient='index')
     return df
 
 
-def get_threshold_table(true_tb, all_tools, max_read, step, rank):
-    tool_list = list(set(all_tools['tool']))
-    thresholds = list(range(0, max_read, step))
+def get_prc_table(true_tb, tool_tb, rank, tool_name, step, end):
     tables = []
-    for threshold in thresholds:
-        for tool_name in tool_list:
-            tool_tb = all_tools[all_tools['tool'] == tool_name]
-            tb = get_precision_recall_f1(true_tb, tool_tb[tool_tb['read_counts'] >= threshold], rank, tool_name)
-            threshold_list=[threshold] * len(tb)
-            tb.insert(len(tb.columns),'threshold',threshold_list)
-            tables.append(tb)
-    pr_tb = pd.concat(tables)
-    return pr_tb
+    for threshold in range(0, end+step, step):
+        tb = get_precision_recall_f1(true_tb, tool_tb, rank, tool_name, threshold)
+        tables.append(tb)
+    tables = pd.concat(tables)
 
-def get_auprc(table):
-    precision=list(table['precision'])
-    recall=list(table['recall'])
-    ap=[]
-    for n in range(1,len(precision)):
-        ap.append(precision[n]*(recall[n]-recall[n-1]))
-    auc=round(abs(sum(ap)),2)
-    return auc
+    precisions = list(tables['precision'])
+    recalls = list(tables['recall'])
+    ap = [precisions[n] * (recalls[n] - recalls[n - 1]) for n in range(1, len(precisions))]#average precision
+    aupr = round(abs(sum(ap)), 2)
+    tables['AUPR'] = [aupr] * len(tables)
+    return tables
 
-pr_table=get_threshold_table(true_superkingdom,tools_superkingdom,max_threshold+read_step,read_step,rank)
+gb_true=true_superkingdom.groupby(['tool','sample',f'{rank}_taxid'],as_index=False).sum()
+gb_tools=tools_superkingdom.groupby(['tool','sample',f'{rank}_taxid'],as_index=False).sum()
 
 subset=[]
-tool_list=list(set(pr_table['tool']))
-for tool in tool_list:
-        table=pr_table[pr_table['tool']==tool]
-        aupr=get_auprc(table)
-        aupr_list=[aupr]*len(table)
-        table.insert(len(table.columns),'AUPR',aupr_list)
+for tool in sorted(tool_list):
+        tool_tax=gb_tools[gb_tools['tool']==tool]
+        table=get_prc_table(gb_true,tool_tax,rank,tool,read_step,max_threshold)
         subset.append(table)
 subset=pd.concat(subset,sort=False)
 sorted_subset=subset.sort_values(by=['AUPR','tool'],ascending=False)
