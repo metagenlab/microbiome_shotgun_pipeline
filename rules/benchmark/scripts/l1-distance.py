44a4f9b6a19f559fd709727d4a2801f86c897a8d
idfarbanecha <faridchaabane23@gmail.com>
2020-06-23 15:27:35 +0200

    tool dict now in config file

--- rules/benchmark/scripts/l1-distance.py
+++ rules/benchmark/scripts/l1-distance.py
@@ -36,14 +36,15 @@ def get_l1_distance(true_tb,tool_tb,tool_name,rank):
     ljoin_all=pd.concat(tables,sort=False)
     return ljoin_all
 
-tool_type={'surpi':'mapping','pathseq':'mapping','ezvir':'mapping','ganon':'k-mer','kraken2':'k-mer','kraken2x':'k-mer','kaiju':'k-mer','bracken':'k-mer','centrifuge':'k-mer'}
+tool_type=snakemake.params.type
 all_tools_l1=[]
 for file in tables:
-    tool = file.split('/')[1]
+    tool = file.split('/')[1].split('.tsv')[0]
+    type = tool_type[tool]
     tb=pd.read_csv(file,sep='\t')
     tool_tb=tb[tb.superkingdom==superkingdom]
     tool_l1 = get_l1_distance(true_superkingdom, tool_tb, tool, rank)
-    tool_l1['type'] = [tool_type[tool]] * len(tool_l1)
+    tool_l1['type'] = [type] * len(tool_l1)
     all_tools_l1.append(tool_l1)
 
 all_tools=pd.concat(all_tools_l1,sort=False)
@@ -51,8 +52,9 @@ all_tools=all_tools.dropna()
 all_tools.reset_index(drop=True,inplace=True)
 all_tools.to_csv(snakemake.output.l1_dist_table,sep='\t',index=False)
 plt.figure(figsize=(11.7,8.27))
-plt.subplots_adjust(right=0.81)
+plt.subplots_adjust(right=0.81,bottom = 0.3)
 sns.set_context('paper',font_scale=1.5)
 vp=sns.violinplot(data=all_tools,x='tool',y='l1_distance',hue='type',dodge=False,cut=0)
+vp.set_xticklabels(vp.get_xticklabels(),rotation=90)
 vp.legend(loc=2, bbox_to_anchor=(1.05, 1),borderaxespad=0.1)
 vp.get_figure().savefig(snakemake.output.l1_distance)
