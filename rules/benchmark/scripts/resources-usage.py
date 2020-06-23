87480aea9ef71af1bbe1f594910b62b22d56d64c
idfarbanecha <faridchaabane23@gmail.com>
2020-06-23 15:32:27 +0200

    added xtick rotation and dict from config file

--- rules/benchmark/scripts/resources-usage.py
+++ rules/benchmark/scripts/resources-usage.py
@@ -6,7 +6,7 @@ import numpy as np
 resources=snakemake.input
 
 tb_list=[]
-tool_type={'surpi':'mapping','pathseq':'mapping','ezvir':'mapping','ganon':'k-mer','kraken2':'k-mer','kraken2x':'k-mer','kaiju':'k-mer','bracken':'k-mer','centrifuge':'k-mer'}
+tool_type=snakemake.params.type
 for file in resources:
     tool=file.split('/')[3].split('.')[0]
     name=file.split('/')[2]
@@ -29,14 +29,21 @@ all_tools.to_csv(snakemake.output.tab,sep='\t')
 plt.figure(figsize=(11.7,8.27))
 bpm=sns.catplot(data=all_tools,x='tool',y='max_mem_gb',col='type',kind='bar',estimator=mean,log=True)
 bpm.set(ylabel='GB')
+for ax in bpm.axes.flat:
+    for label in ax.get_xticklabels():
+        label.set_rotation(90)
 bpm.savefig(snakemake.output.memo)
 
 plt.figure(figsize=(11.7,8.27))
 bpt=sns.catplot(data=all_tools,x='tool',y='minutes',col='type',kind='bar',estimator=mean)
 bpt.set(ylabel='minutes')
+for ax in bpt.axes.flat:
+    for label in ax.get_xticklabels():
+        label.set_rotation(90)
 bpt.savefig(snakemake.output.time)
 
 fig = plt.figure(figsize=(11.7,8.27))
+plt.subplots_adjust(bottom = 0.3)
 plt.rcParams.update({'font.size': 14})
 ax = fig.add_subplot(111)
 ax2 = ax.twinx()
@@ -50,7 +57,7 @@ ps=[]
 ps.append(ax.bar(data=gb,x=np.arange(N)*dim + offsets[0],height='max_mem_gb',color='salmon',label='memory (GB)'))
 ps.append(ax2.bar(data=gb,x=np.arange(N)*dim + offsets[1],height='minutes',color='pink',label='minutes'))
 ax.set_xticks(np.arange(N) * dim)
-ax.set_xticklabels([tool for tool in gb['tool']])
+ax.set_xticklabels([tool for tool in gb['tool']],rotation=90)
 ax.legend(ps,['memory','time'],loc='upper left')
 ax.set_ylabel('Gigabytes')
 ax2.set_ylabel('Minutes')
