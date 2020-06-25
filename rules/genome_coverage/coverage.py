2d8fdb9006a9fc6d6b66024a01becec37e63c57b
idfarbanecha <faridchaabane23@gmail.com>
2020-06-25 15:13:35 +0200

    improved coverage plot

--- rules/genome_coverage/coverage.py
+++ rules/genome_coverage/coverage.py
@@ -8,6 +8,7 @@ Entrez.api_key=snakemake.params.NCBI_key
 samplename=snakemake.wildcards.sample
 fullname=snakemake.wildcards.filename
 
+
 temp=fullname.split('_')
 acc='_'.join(temp[:2])#split on the second underscore to get accession id
 try:
@@ -32,10 +33,17 @@ reads_mapped=info.split('\n')[4].split(' ')[0]
 
 covered=len(coverage[coverage['cov']!=0])#Coverage = positions that are mapped by at least one read
 covpercent=round((covered/len(coverage))*100,2)#Coverage percent = Coverage/genome length *100 (rounded to 2 decimals)
+med_reads=round(coverage['cov'].median(),2)#Calculate median read coverage
+rollm=coverage.rolling(len(coverage)//2).mean()#calculate moving average with a sliding window of genome length divided by 100
 
+plt.style.use('ggplot')
 plt.figure(figsize=(11.7,8.27))
-plt.title(f'{samplename} reads mapped to {name}, assembly accession: {accession}\n\n {covpercent}% covered\n\n total reads mapped:{reads_mapped}',fontsize=12,ha='center')
-plt.plot('pos','cov',data=coverage)
-plt.xlabel('position',fontsize=11)
-plt.ylabel('depth',fontsize=11)
-plt.savefig(snakemake.output[0])
+plt.title(f'{samplename} reads mapped to {name}, assembly accession: {accession}\n\n{covpercent}% covered',fontsize=14,ha='center')
+plt.gcf().text(0.92, 0.5,f'Total reads mapped: {reads_mapped}\n\nMedian reads mapped: {med_reads}',fontsize=12,bbox={'facecolor':'white','alpha':0.7,'edgecolor': 'black','pad':0.5,'boxstyle':'round'},style='italic')
+plt.plot('pos','cov',data=coverage,c='darkblue',alpha=0.2,label='read number')
+plt.plot('pos','cov',data=rollm,c='blue',label='rolling mean')
+plt.legend(loc=2, bbox_to_anchor=(1.05, 1),borderaxespad=0.1)
+plt.ylabel('depth')
+plt.xlabel('position')
+plt.xlim(0,len(rollm))
+plt.savefig(snakemake.output[0],bbox_inches='tight')
