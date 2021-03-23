7bdc3447f54df632d5db49f31b9eab6e3e1184ca
idfarbanecha <faridchaabane23@gmail.com>
2021-03-23 23:16:44 +0100

    added unclassified reads in tax table

--- rules/benchmark/scripts/kraken2-tax.py
+++ rules/benchmark/scripts/kraken2-tax.py
@@ -2,19 +2,10 @@ import pandas as pd
 from Bio import Entrez
 from ete3 import NCBITaxa
 import numpy as np
-import os
-from datetime import datetime
+
 Entrez.email=snakemake.params.NCBI_email
 Entrez.api_key=snakemake.params.NCBI_key
 ncbi = NCBITaxa()
-sqldb=os.path.join(os.environ['HOME'],'.etetoolkit','taxa.sqlite')#path to ete sql db
-db_modification_time=datetime.fromtimestamp(os.path.getctime(sqldb))
-current_time=datetime.now()
-database_age_days=abs((db_modification_time-current_time).days)
-if database_age_days>30:
-    ncbi.update_taxonomy_database()
-
-
 
 def get_lin_tax(tab, ncbi, target_ranks):
     tax = {}
@@ -25,10 +16,9 @@ def get_lin_tax(tab, ncbi, target_ranks):
     count_dic = dict(zip(taxid_list, counts))
     percent_dic = dict(zip(taxid_list, percents))
     for taxid in taxid_list:
-        tax[taxid]={}
+        tax[taxid] = {'taxid': str(int(taxid)), 'read_percent': percent_dic[taxid],
+                      'read_counts': str(int(count_dic[taxid]))}
         if int(taxid) > 0 and count_dic[taxid] > 0:
-            tax[taxid] = {'taxid': str(int(taxid)), 'read_percent': percent_dic[taxid],
-                          'read_counts': str(int(count_dic[taxid]))}
             lineage=None
             scientific_name = ncbi.translate_to_names([taxid])[0]
             tax[taxid]['scientific_name'] = scientific_name
@@ -41,7 +31,8 @@ def get_lin_tax(tab, ncbi, target_ranks):
                     del_entry_name = Entrez.read(Entrez.esummary(db='taxonomy', id=f'{taxid}'))[0]['ScientificName']
                     print(f'deleted entry name: {del_entry_name}')
                     simple_name = ' '.join(del_entry_name.split(' ')[
-                                           0:2])  # Some times, subspecies is added to the scientific name, and ete3 cannot find a match
+                                           0:2])
+                    # Some times, subspecies is added to the scientific name,and ete3 cannot find a match
                     print(f'first two words of entry name {simple_name}')
                     if 'unclassified' in simple_name:
                         print('discarding "unclassified" in name')
@@ -82,14 +73,20 @@ def get_lin_tax(tab, ncbi, target_ranks):
                             if previous_rank not in rank2names.keys():
                                 continue
                         tax[taxid][rank] = f'{previous_name}_{rank[0:1]}'
+        if int(taxid) == 0 and count_dic[taxid] > 0:
+            tax[taxid]['scientific_name'] = 'unclassified'
     df = pd.DataFrame.from_dict(tax, orient='index')
     return df
 
+
 target_ranks = ['superkingdom','phylum','order','family','genus','species']
-sample_tb=pd.read_csv(snakemake.input[0],sep='\t',names=["read_percent","rooted_frag_num","reads_assigned","rank_code","taxid","scientific_names"])
+sample_tb=pd.read_csv(snakemake.input[0],sep='\t',names=["read_percent","rooted_frag_num","reads_assigned",
+                                                         "rank_code","taxid","scientific_names"])
 lin_tax_tb=get_lin_tax(sample_tb,ncbi,target_ranks)
-lin_tax_tb['read_counts']=np.int64(lin_tax_tb['read_counts'])
+lin_tax_tb=lin_tax_tb.astype({'read_counts':'int64','taxid':'object'})
 lin_tax_tb['sample']=[snakemake.wildcards.sample]*len(lin_tax_tb)
-linear_taxonomy_table=lin_tax_tb[['superkingdom','superkingdom_taxid','phylum','phylum_taxid','order','order_taxid','family','family_taxid','genus','genus_taxid','species','species_taxid','scientific_name','taxid','read_counts','read_percent','sample']]
+linear_taxonomy_table=lin_tax_tb[['superkingdom','superkingdom_taxid','phylum','phylum_taxid','order',
+                                  'order_taxid','family','family_taxid','genus','genus_taxid','species','species_taxid',
+                                  'scientific_name','taxid','read_counts','read_percent','sample']]
 linear_taxonomy_table.replace(np.nan,'na',inplace=True)
 linear_taxonomy_table.to_csv(snakemake.output[0], sep='\t',index=False)
