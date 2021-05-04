e651163e3c54a194db54daa00016b309036d334e
idfarbanecha <faridchaabane23@gmail.com>
2021-05-04 16:30:49 +0200

    improved kraken2 logging

--- rules/benchmark/scripts/kraken2-tax.py
+++ rules/benchmark/scripts/kraken2-tax.py
@@ -2,10 +2,14 @@ import pandas as pd
 from Bio import Entrez
 from ete3 import NCBITaxa
 import numpy as np
-
-Entrez.email=snakemake.params.NCBI_email
-Entrez.api_key=snakemake.params.NCBI_key
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d %b %Y %H:%M:%S',
+                    filename=snakemake.log[0], level=logging.DEBUG)
+logging.captureWarnings(True)
+Entrez.email = snakemake.params.NCBI_email
+Entrez.api_key = snakemake.params.NCBI_key
 ncbi = NCBITaxa()
+
 
 def get_lin_tax(tab, ncbi, target_ranks):
     tax = {}
@@ -16,9 +20,9 @@ def get_lin_tax(tab, ncbi, target_ranks):
     count_dic = dict(zip(taxid_list, counts))
     percent_dic = dict(zip(taxid_list, percents))
     for taxid in taxid_list:
-        tax[taxid] = {'taxid': str(int(taxid)), 'read_percent': percent_dic[taxid],
-                      'read_counts': str(int(count_dic[taxid]))}
         if int(taxid) > 0 and count_dic[taxid] > 0:
+            tax[taxid] = {'taxid': str(int(taxid)), 'read_percent': percent_dic[taxid],
+                          'read_counts': str(int(count_dic[taxid]))}
             lineage=None
             scientific_name = ncbi.translate_to_names([taxid])[0]
             tax[taxid]['scientific_name'] = scientific_name
@@ -26,28 +30,28 @@ def get_lin_tax(tab, ncbi, target_ranks):
                 lineage = ncbi.get_lineage(taxid)
                 foundlineage = True
             except ValueError:
-                print(f'taxid:{taxid} not found, searching NCBI taxonomy')
+                logging.error(f'taxid:{taxid} not found, searching NCBI taxonomy')
                 try:
                     del_entry_name = Entrez.read(Entrez.esummary(db='taxonomy', id=f'{taxid}'))[0]['ScientificName']
-                    print(f'deleted entry name: {del_entry_name}')
+                    logging.info(f'deleted entry name: {del_entry_name}')
                     simple_name = ' '.join(del_entry_name.split(' ')[
                                            0:2])
                     # Some times, subspecies is added to the scientific name,and ete3 cannot find a match
-                    print(f'first two words of entry name {simple_name}')
+                    logging.info(f'first two words of entry name {simple_name}')
                     if 'unclassified' in simple_name:
-                        print('discarding "unclassified" in name')
+                        logging.info('discarding "unclassified" in name')
                         simple_name = simple_name.split('unclassified ')[1]
-                        print(f'final name: {simple_name}')
+                        logging.info(f'final name: {simple_name}')
                     if 'incertae' in simple_name:
-                        print('discarding "incertae:" in name')
+                        logging.info('discarding "incertae:" in name')
                         simple_name = simple_name.split('incertae')[0].strip()
-                        print(f'final name: {simple_name}')
+                        logging.info(f'final name: {simple_name}')
                     updated_taxid = list(ncbi.get_name_translator([simple_name]).values())[0][0]
-                    print("updated_taxid", updated_taxid)
+                    logging.info(f"updated_taxid: {updated_taxid} ")
                     lineage = ncbi.get_lineage(updated_taxid)
                     foundlineage = True
                 except RuntimeError:
-                    print("Could not convert to ncbi taxid, skipping:", tax[taxid])
+                    logging.error(f"Could not convert to ncbi taxid, skipping:{tax[taxid]}")
                     foundlineage = False
 
             if foundlineage:
@@ -74,19 +78,26 @@ def get_lin_tax(tab, ncbi, target_ranks):
                                 continue
                         tax[taxid][rank] = f'{previous_name}_{rank[0:1]}'
         if int(taxid) == 0 and count_dic[taxid] > 0:
+            tax[taxid] = {'taxid': str(int(taxid)), 'read_percent': percent_dic[taxid],
+                          'read_counts': str(int(count_dic[taxid]))}
             tax[taxid]['scientific_name'] = 'unclassified'
     df = pd.DataFrame.from_dict(tax, orient='index')
     return df
 
 
-target_ranks = ['superkingdom','phylum','order','family','genus','species']
-sample_tb=pd.read_csv(snakemake.input[0],sep='\t',names=["read_percent","rooted_frag_num","reads_assigned",
+target_ranks = ['strain', 'species', 'genus', 'family', 'order', 'class', 'phylum', 'superkingdom']
+sample_tb = pd.read_csv(snakemake.input.tax,sep='\t',names=["read_percent","rooted_frag_num","reads_assigned",
                                                          "rank_code","taxid","scientific_names"])
-lin_tax_tb=get_lin_tax(sample_tb,ncbi,target_ranks)
-lin_tax_tb=lin_tax_tb.astype({'read_counts':'int64','taxid':'object'})
+sample_types = pd.read_csv(snakemake.input.sample_info,sep='\t')[['SampleName','bodysite','type']]
+sample_types = sample_types.rename(columns={'SampleName':'sample'})
+lin_tax_tb = get_lin_tax(sample_tb,ncbi,target_ranks)
+lin_tax_tb = lin_tax_tb.astype({'read_counts':'int64','taxid':'object'})
 lin_tax_tb['sample']=[snakemake.wildcards.sample]*len(lin_tax_tb)
-linear_taxonomy_table=lin_tax_tb[['superkingdom','superkingdom_taxid','phylum','phylum_taxid','order',
+addtypes=pd.merge(lin_tax_tb, sample_types, how='left', on='sample')
+
+linear_taxonomy_table = addtypes[['superkingdom','superkingdom_taxid','phylum','phylum_taxid','order',
                                   'order_taxid','family','family_taxid','genus','genus_taxid','species','species_taxid',
-                                  'scientific_name','taxid','read_counts','read_percent','sample']]
-linear_taxonomy_table.replace(np.nan,'na',inplace=True)
+                                  'strain', 'strain_taxid','scientific_name','taxid','read_counts','read_percent',
+                                  'sample','bodysite','type']]
+linear_taxonomy_table.replace(np.nan, 'na', inplace=True)
 linear_taxonomy_table.to_csv(snakemake.output[0], sep='\t',index=False)
