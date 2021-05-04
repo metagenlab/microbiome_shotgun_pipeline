669573c488a4da58c919c5eb86a0bad1b01ea35d
idfarbanecha <faridchaabane23@gmail.com>
2021-05-04 16:39:08 +0200

    improved ete3 logging

--- rules/benchmark/scripts/update-ete3.py
+++ rules/benchmark/scripts/update-ete3.py
@@ -1,18 +1,18 @@
+import sys
 import os
 from datetime import datetime
 from ete3 import NCBITaxa
-import logging
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%d %b %Y %H:%M:%S',
-                    filename=snakemake.log[0], level=logging.DEBUG)
-ncbi = NCBITaxa()
-sqldb = os.path.join(os.environ['HOME'], '.etetoolkit', 'taxa.sqlite')  # path to ete sql db
-db_modification_time = datetime.fromtimestamp(os.path.getctime(sqldb))
-database_age_days = abs((db_modification_time-datetime.now()).days)
-if database_age_days > 30:
-    ncbi.update_taxonomy_database()
-    comment = 'taxa.sqlite is more than a month old, updating database. More details in logs/benchmark'
-else:
-    comment = 'taxa.sqlite is up to date'
-file = open(snakemake.output[0], 'w')
-file.write(comment)
-file.close()
+with open(snakemake.log[0], "w") as f:
+    sys.stderr = sys.stdout = f
+    ncbi = NCBITaxa()
+    sqldb = os.path.join(os.environ['HOME'], '.etetoolkit', 'taxa.sqlite')  # path to ete sql db
+    db_modification_time = datetime.fromtimestamp(os.path.getctime(sqldb))
+    database_age_days = abs((db_modification_time-datetime.now()).days)
+    if database_age_days >= 5:
+        ncbi.update_taxonomy_database()
+        comment = f'taxa.sqlite is more than {database_age_days} days old, updating database'
+    else:
+        comment = 'taxa.sqlite is up to date'
+    file = open(snakemake.output[0], 'w')
+    file.write(comment)
+    file.close()
